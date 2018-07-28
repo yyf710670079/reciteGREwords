3ac26caac8868060d8e207060bf84dd2a22497e4
yyf710670079 <32241356+yyf710670079@users.noreply.github.com>
2018-07-28 21:37:31 +0800

    modified

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class App:
     def beidanci(self):
         newPage = Tk()
         newPage.title('背单词模式 author:杨宇锋')
-        newPage.geometry('1150x600')
+        newPage.geometry('1300x700')
 
         records = {}
 
@@ -52,19 +52,19 @@ class App:
         rd = [randint(0, length_csv) for m in range(10)]
 
         def check_whether_recite(old_rd):
-            if 0 == len(records):
-                return old_rd
+            #    if 0 == len(records):
+            #        return old_rd
             for i in range(10):
-                if data[old_rd[i]][0] in records and records[data[old_rd[i]][0]] >= 2:
-                    while data[old_rd[i]][0] in records and records[data[old_rd[i]][0]] >= 2:
+                if data[old_rd[i]][0] in records and records[data[old_rd[i]][0]] >= 1:
+                    while data[old_rd[i]][0] in records and records[data[old_rd[i]][0]] >= 1:
                         old_rd[i] = randint(0, length_csv)
             return old_rd
 
         rd = check_whether_recite(rd)
 
         for m in range(10):
-            var_list[m] = data[rd[m]][0]+' :    '
-            meaning_list[m] = data[rd[m]][1]
+            var_list[m] = data[rd[m]][0] + ' :'
+            meaning_list[m] = data[rd[m]][1][0:100]
 
         def get_sound():
             word_list = [var_list[i].split(' ')[0] for i in range(10)]
@@ -75,7 +75,7 @@ class App:
                 try:
                     track = pygame.mixer.music.load(file)
                     pygame.mixer.music.play()
-                    time.sleep(1.3)
+                    time.sleep(1.8)
                     pygame.mixer.music.stop()
                 except BaseException as e:
                     time.sleep(1.3)
@@ -84,9 +84,9 @@ class App:
         meaning_text = '\n\n'.join(meaning_list)
 
         word_box = Label(newPage, text=var_text, justify=LEFT)
-        word_box.pack(side=LEFT, padx=25)
+        word_box.pack(side=LEFT, padx=40)
 
-        meaning_box = Label(newPage, text=meaning_text, width=91, justify=LEFT)
+        meaning_box = Label(newPage, text=meaning_text, width=100, justify=LEFT)
         meaning_box.pack(side=LEFT)
 
         def get_new_page():
@@ -96,8 +96,8 @@ class App:
             new_rd = [randint(0, length_csv) for m in range(10)]
             new_rd = check_whether_recite(new_rd)
             for m in range(10):
-                var_list[m] = data[new_rd[m]][0] + ' :    '
-                meaning_list[m] = data[new_rd[m]][1]
+                var_list[m] = data[new_rd[m]][0] + ' :'
+                meaning_list[m] = data[new_rd[m]][1][0:100]
             _var_text = '\n\n'.join(var_list)
             _meaning_text = '\n\n'.join(meaning_list)
 
@@ -122,7 +122,7 @@ class App:
     def review(self):
         reviewPage = Tk()
         reviewPage.title('复习模式 author:杨宇锋')
-        reviewPage.geometry('800x400')
+        reviewPage.geometry('1000x500')
 
         review_records = {}
         try:
@@ -152,7 +152,7 @@ class App:
         wordLabel = Label(reviewPage, text=rd_word, font=('宋体', 35))
         wordLabel.pack(padx=40, pady=20)
         meaning = '隐藏释义'
-        meaningLabel = Label(reviewPage, text=meaning, font=('宋体', 17), justify=LEFT)
+        meaningLabel = Label(reviewPage, text=meaning, font=('宋体', 17), justify=LEFT, width=101)
         meaningLabel.pack(padx=40, pady=20)
 
         review_records[rd_word] += 1
@@ -163,7 +163,7 @@ class App:
                 review_records[rd_word] -= 1
 
         respondButton = Button(reviewPage, text='不太熟悉', command=reduce_times, font=('宋体', 20))
-        respondButton.pack(padx=40,pady=20)
+        respondButton.pack(padx=40, pady=20)
 
         def show_low_level():
             global rd_word
@@ -220,12 +220,11 @@ class App:
             try:
                 track1 = pygame.mixer.music.load(file3)
                 pygame.mixer.music.play()
-                time.sleep(1.5)
+                time.sleep(1.8)
                 pygame.mixer.music.stop()
             except BaseException as e:
-                time.sleep(1.5)
+                time.sleep(1)
                 print('No sound of this word')
-
 
         explaination = Button(reviewPage, text='查看释义', font=('宋体', 20), command=showMeaning)
         explaination.pack(padx=80, pady=20)
